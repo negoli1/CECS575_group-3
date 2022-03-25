12c326cb4e78231de3b16f563eb656a966ee4cb8
GopiKiranKommineni <53233342+GopiKiranKommineni@users.noreply.github.com>
2022-03-24 21:14:24 -0700

    Add files via upload

---
+++ Code/Merged Code/User.java
@@ -0,0 +1,62 @@
+package factory;
+
+import java.math.BigInteger;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private BigInteger mobileNumber;
+	private int userID;
+	private String userType;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public BigInteger getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(BigInteger mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+}
